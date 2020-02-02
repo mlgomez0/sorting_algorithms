f208a88a8b226c7a207e3a41973e0d4042706a28
mlgomez0 <mlgomez230@hotmail.com>
2020-02-02 21:14:12 +0000

    100- first commit

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ *shell_sort - sort an array using the shell_sort algorithm
+ *@array:array to be sorted
+ *@size:size of the array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	int i, temp, j, gap = 1;
+	int n = size;
+
+	while (gap < n / 3)
+	{
+		gap = 3 * gap + 1;
+	}
+	while (gap > 0)
+	{
+		for (i = gap; i < n; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
