1cf408afad8adb956502a6fe452d60d78513755a
mlgomez0 <mlgomez230@hotmail.com>
2020-02-02 01:40:31 +0000

    3- second

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,25 +13,27 @@ void quick_sort(int *array, size_t size)
 	quicksort(array, 0, n, size);
 }
 
-int partition_step(int *array, int lo, int hi)
+int partition_step(int *array, int lo, int hi, size_t size)
 {
 	int pivot = array[hi];
-	int i = lo - 1, temp, j;
+	int i = lo, temp, j;
 
-	for (j = lo; j <= hi - 1; j++)
+	for (j = lo; j <= hi; j++)
 	{
 		if (array[j] < pivot)
 		{
-			i++;
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
+			print_array(array, size);
+			i++;
 		}
 	}
 	temp = array[hi];
-	array[hi] = array[i + 1];
-	array[i + 1] = temp;
-	return (i + 1);
+	array[hi] = array[i];
+	array[i] = temp;
+	print_array(array, size);
+	return (i);
 }
 
 void quicksort(int *array, int lo, int hi, size_t size)
@@ -40,8 +42,7 @@ void quicksort(int *array, int lo, int hi, size_t size)
 
 	if (lo < hi)
 	{
-		p = partition_step(array, lo, hi);
-		print_array(array, size);
+		p = partition_step(array, lo, hi, size);
 		quicksort(array, lo, p - 1, size);
 		quicksort(array, p + 1, hi, size);
 	}
--- sort.h
+++ sort.h
@@ -26,7 +26,7 @@ void insert_middle(listint_t *sort, listint_t *prog);
 void insert_uniq(listint_t *sort, listint_t *prog);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-int partition_step(int *array, int lo, int hi);
+int partition_step(int *array, int lo, int hi, size_t size);
 void quicksort(int *array, int lo, int hi, size_t size);
 
 #endif
